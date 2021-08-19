c32bc139e664f82529deea03ce4dd69fe2c6e1d5
Ravi <marsstrikers@gmail.com>
2021-08-19 16:06:52 +0545

    Password CSS Added.

--- php/changePassword.php
+++ php/changePassword.php
@@ -5,30 +5,48 @@
 
 <head>
     <title>Change Password</title>
+    <style>
+        <?php include '../css/bootstrap.min.css'; ?>
+        <?php include '../css/changePassword.css'; ?>
+    </style>
 </head>
 
 <body>
-    <div>
-        <h1>Change Your Password</h1>
+   <div class ="container mt-5 mb-5 d-flex justify-content-center"> 
+        <div class ="card-head">
+            <h1 class="card-title mb-3">Institute of Engineering Pulchowk,Campus
+            </h1>
+            <hr>
+            <h3 class="card-detail mb-3">Change Your Password</h3>
+        </div>
+        <div class="row-body">
+            <form method="POST" action="changePasswordIndp.php">
+                <div class="form-row col-md-8">
+                    
+                        <label for="inputcurrentpassword4">Current Password:</label>
+                        <input class ="form-control" type="text" name="password" placeholder="Current Password">
+                    
+                </div>
+                <div class="form-row col-md-8">
+                    
+                    <label for="inputnewPass4">New Password:</label>
+                        <input class ="form-control" type="password" name="newPass1" placeholder="New Password">
+                    
+                </div>
+                <div class="form-row col-md-8">
+                    
+                        <label for="inputnewPass24">Re-enter New Password:</label>
+                        <input class ="form-control" type="password" name="newPass2" placeholder="Re-enter New Password">
+                    
+                </div>
+                <div class="form-row col-md-8">
+                    <div class="confirm-btn">
+                        <input class ="btn btn-lg btn-primary" type="submit" value="Confirm">
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-
-    <div>
-        <form method="POST" action="changePasswordIndp.php">
-            <div>
-                <input type="text" name="password" placeholder="Current Password">
-            </div>
-            <div>
-                <input type="password" name="newPass1" placeholder="New Password">
-            </div>
-            <div>
-                <input type="password" name="newPass2" placeholder="Re-enter New Password">
-            </div>
-            <div>
-                <input type="submit" value="Confirm">
-            </div>
-        </form>
-    </div>
-
 </body>
 
 </html>
